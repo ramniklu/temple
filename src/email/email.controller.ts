f6e0d046c473a0381cb73451323b2dbf459bf43c
ramniklu <155295174+ramniklu@users.noreply.github.com>
2024-01-06 16:22:22 +0500

    Update email.controller.ts

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -12,21 +12,45 @@ export class EmailController {
     private readonly userService: UsersService,
   ) { }
 
+  // @Post('send/bulk')
+  // @UseInterceptors(FileInterceptor('attachments')) // Use FileInterceptor for handling attachments
+  // async sendBulkEmail(
+  //   @Body() data: any,
+  //   @UploadedFiles() attachments: Express.Multer.File[], // Attachments from request
+  // ): Promise<void> {
+  //   let users = [];
+
+  //   if (!data || !data.users || !Array.isArray(data.users) || data.users.length === 0) {
+  //     users = (await this.userService.findAllUsers()).map(x => x.email);
+  //     data.users = users;
+  //   }
+
+  //   await this.emailService.sendBulkEmail(data, attachments); // Pass attachments to the service
+  // }
   @Post('send/bulk')
-  @UseInterceptors(FileInterceptor('attachments')) // Use FileInterceptor for handling attachments
-  async sendBulkEmail(
-    @Body() data: any,
-    @UploadedFiles() attachments: Express.Multer.File[], // Attachments from request
-  ): Promise<void> {
-    let users = [];
+@UseInterceptors(FileInterceptor('attachments'))
+async sendBulkEmail(
+  @Body() data: any,
+  @UploadedFiles() attachments: Express.Multer.File[],
+): Promise<void> {
+  let users = [];
 
-    if (!data || !data.users || !Array.isArray(data.users) || data.users.length === 0) {
-      users = (await this.userService.findAllUsers()).map(x => x.email);
-      data.users = users;
-    }
-
-    await this.emailService.sendBulkEmail(data, attachments); // Pass attachments to the service
+  if (!data || !data.users || !Array.isArray(data.users) || data.users.length === 0) {
+    users = (await this.userService.findAllUsers()).map(x => x.email);
+    data.users = users;
   }
+
+  if (!attachments) {
+    // Handle the case when 'attachments' is undefined or empty
+    // You can log an error, throw an exception, or handle it according to your requirements
+    console.error('No attachments provided.');
+    // Optionally, you can return a 400 Bad Request response
+    throw new BadRequestException('No attachments provided.');
+  }
+
+  await this.emailService.sendBulkEmail(data, attachments);
+}
+
 
   @Get('template/list')
   async listTemplate() {
